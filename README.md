4c6e3a3a7afa9d16855be4674d7e9d860ada653f
宮川優斗 <miyakaway7@gmail.com>
2019-09-28 19:53:41 +0900

    来週の月曜も日報を生成できるようにした

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 自作したシェルスクリプトをまとめるためのrepository。
 
 コマンド化する場合は、/usr/local/binディレクトリにシンボリックリンクを作成して対応する。
-
+https://qiita.com/genya0228/items/01b63d449a6c1956df7d
--- generate-daily-repo.sh
+++ generate-daily-repo.sh
@@ -3,13 +3,29 @@
 ## ファイル名: generate-daily-repo.sh
 ## 処理内容: 日報の生成を行う
 
+echo '生成する日報を選択してください'
+select daily_repo_type in '明日' '来週月曜' '今日'
+do
+	echo "${daily_repo_type}の日報を作成しますか？[Y/n]: "
+	read -r confirm
+	if [[ ($confirm =~ [Yy]) || ($confirm == '') ]]; then
+		break
+	else
+		echo '日報を生成しないで終了します'
+		exit 1
+	fi
+done
+
 # ファイル名設定
 # shellcheck disable=SC2154
 today="$(date +%Y%m%d)" # 今日の日付取得
 tomorrow="$(date -v+1d '+%Y%m%d')"
-ask_create_tommorow_repo='明日の日報を作成しますか？[Y/n]: '
-file_today="$(date +%Y-%m-%d)" # 日報の方で使用する日付(-付き)
-file_tomorrow="$(date -v+1d +%Y-%m-%d)" # 日報の方で使用する日付(-付き)
+next_monday="$(date -v-monday -v+7d +'%Y%m%d')" # 来週の月曜
+
+# 日報の方で使用する日付(-付き)
+file_today="$(date +%Y-%m-%d)"
+file_tomorrow="$(date -v+1d +%Y-%m-%d)"
+file_next_monday="$(date -v-monday -v+7d +'%Y-%m-%d')"
 
 # 引数で指定された曜日を返す
 get_day_of_week() {
@@ -55,6 +71,41 @@ EOS
 	fi
 }
 
+generate_next_monday_daily_repo() {
+	local dowIdx
+	dowIdx="$(date -v-monday -v+7d '+%w')"
+	echo "${dowIdx}"
+	local dow
+	# 曜日の決定
+	dow="$(get_day_of_week "${dowIdx}")"
+	echo "${dow}"
+	# ファイルが存在していない時のみ作成
+	if [[ -e "${HOME}/Desktop/AutoScale-for-me/daily-repo/${next_monday}日報.md" ]]; then
+		echo "${HOME}/Desktop/AutoScale-for-me/daily-repo/${next_monday}日報.mdは存在しています"
+		exit 1
+	else
+		cat << EOS > "${HOME}/Desktop/AutoScale-for-me/daily-repo/${next_monday}日報.md"
+
+${file_next_monday} ($dow)
+
+予定
+${file_next_monday} ($dow)
+- 10:00 出社
+- 10:00 - 13:00
+- 13:00 - 14:00 休憩
+- 18:30 - 19:00 次回の準備
+
+実績
+${file_next_monday} ($dow)
+- 10:00 出社
+- 10:00 - 13:00
+- 13:00 - 14:00 休憩
+- 18:30 - 19:00 次回の準備
+
+EOS
+	fi
+}
+
 generate_today_daily_repo() {
 	local dowIdx
 	dowIdx="$(date '+%w')"
@@ -90,16 +141,22 @@ EOS
 	fi
 }
 
-# 明日or今日の日報どちらの日報を作成するか尋ねる
-echo "${ask_create_tommorow_repo}"
-read -r answer_create_tommorow_repo
-case "${answer_create_tommorow_repo}" in
-	'' | [Yy]* )
+# 選択されたタイプの日報を作成する
+case "${daily_repo_type}" in
+	'明日' )
 		echo '明日の日報を作成しました'
 		generate_tommorow_daily_repo
 		;;
-	* )
+	'来週月曜' )
+		echo '来週月曜の日報を作成しました'
+		generate_next_monday_daily_repo
+		;;
+	'今日' )
 		echo '今日の日報を作成しました'
 		generate_today_daily_repo
+		;;
+	* )
+		echo '予期しない動作です。'
+		exit 1
 		;;
 esac
